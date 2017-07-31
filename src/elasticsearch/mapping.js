6910f623ba91c19fc82960e49bbbe611acd10fa9
Thanh Nguyen <npt.thanh105@gmail.com>
2017-07-31 23:10:35 +0700

    Check execute for mapping

--- src/elasticsearch/mapping.js
+++ src/elasticsearch/mapping.js
@@ -5,12 +5,14 @@ import fs from 'fs';
 const router = express.Router();
 const searchIndexDir = __dirname + "\\routes\\";
 
+const execute = [];
+
 // Read each file in the routes directory
 fs.readdirSync(searchIndexDir).forEach(function (route) {
   // Strip the .js suffix
   route = route.split('.')[0];
   // Ignore index (i.e. this file)
-  if (route === 'index') return;
+  if (route === 'index' || execute.indexOf(route) !== -1) return;
   console.log('Loading route ' + route + '...');
   // Mount router
   router.use('/' + route, require(searchIndexDir + route + '.js'));
